360ede7a096f766e7b3f8fd78398d57336c07796
yelin5262 <yelin5262@gmail.com>
2022-05-06 00:48:30 +0900

    외부 링크 비디오 재생

--- MoviePlayer/ViewController.swift
+++ MoviePlayer/ViewController.swift
@@ -30,7 +30,17 @@ class ViewController: UIViewController {
         }
     }
     @IBAction func btnPlayExternalMovie(_ sender: UIButton) {
-    
+        // 외부 파일 mp4
+        let url = NSURL(string: "https://dl.dropboxusercontent.com/s/e38auz050w2mvud/Fireworks.mp4")!
+        
+        let playerController = AVPlayerViewController()
+        
+        let player = AVPlayer(url: url as URL)
+        playerController.player = player
+        
+        self.present(playerController, animated: true) {
+            player.play()
+        }
     }
     
 }
